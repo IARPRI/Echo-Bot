4d7f5f06a9c836f767c212db14979c1f73a40677
Iaroslav Pripisnov <realiarpri@gmail.com>
2023-03-18 19:27:08 +0300

    v1 simple echo bot

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-# Build on Python 3.11.2
-# First you must install next two libs: requests, aiogram.
-# Recognize token in bottokenhere.txt file
+# Build on Python 3.11.2 и aiogram 3.0.0b7
+# First you must install next two libs: requests, aiogram (ver 3+).
 
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = 'BOT TOKEN HERE'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=["help"]))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
